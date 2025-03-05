f1b2f97e747c37ef1b45b36afef1b2277b59c88f
NaufalArkaan <naufalarkaan25@gmail.com>
2025-03-05 22:28:47 +0700

    latihan modul_2 new

---
+++ Latihan/Modul_2/Farmer.java
@@ -0,0 +1,8 @@
+public class Farmer {
+    String name;
+    String favourite;
+
+    void talk(){
+        System.out.println("Hi, my name is: " + name + ". My favourite plants is: " + favourite);
+    }
+}
--- Latihan/Modul_2/Main.java
+++ Latihan/Modul_2/Main.java
@@ -1,9 +1,23 @@
-public class Main {
-    static String mengerem(){
-        return "Berhenti";
-    }
+import java.util.Date;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Andi membawa mobil lalu dia " + mengerem());
+        Farmer farmer1 = new Farmer();
+        Farmer farmer2 = new Farmer();
+        Plant plant1 = new Plant();
+        Plant plant2 = new Plant();
+
+        farmer1.name = "Naufal";
+        farmer2.name = "Arkaan";
+        plant1.name = "Sun flower";
+        plant2.name = "Mushroom";
+
+        farmer1.favourite = plant1.name;
+        farmer2.favourite = plant2.name;
+
+
+        System.out.println("Current date and time: " + new Date());
+        farmer1.talk();
+        farmer2.talk();
     }
 }
