16aa95d0cd43b12e22a9fa2a7fa0fe557941db35
NaufalArkaan <naufalarkaan25@gmail.com>
2025-03-05 12:50:32 +0700

    Second latihan modul_2

--- Latihan/Modul_2/Main.java
+++ Latihan/Modul_2/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    static String mengerem(){
+        return "Berhenti";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Ini modul 2 latihan");
+        System.out.println("Andi membawa mobil lalu dia " + mengerem());
     }
 }
